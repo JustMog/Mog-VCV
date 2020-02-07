4d092d593551ccf3a25ca98aed7d43b501c60d8d
JustMog <g-seddon@outlook.com>
2020-02-07 21:44:54 +0000

    allow save & load, undo, randomize etc. for bypass buttons

--- src/plugin.hpp
+++ src/plugin.hpp
@@ -2,6 +2,9 @@
 #include <rack.hpp>
 #include "mog_components.hpp"
 
+// Explicit <array> include required on OS X
+#include <array> 
+
 using namespace rack;
 
 // Declare the Plugin, defined in plugin.cpp
@@ -10,7 +13,7 @@ extern Plugin* pluginInstance;
 
 // Declare each Model, defined in each module source file
 // extern Model* modelMyModule;
-extern Model* modelQuantizer;
+//extern Model* modelQuantizer;
 extern Model* modelNetwork;
 
 
